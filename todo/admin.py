2def2b4e2a4ea2e8ccfb2682822e730514f0ffd2
100ballovby <legorobotsbel@gmail.com>
2023-02-25 15:38:42 +0300

    Adding todos and priority models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Task, Priority
+
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'priority', 'user_id', 'deadline_date',]
+    list_filter = ['priority', 'status', 'user_id',]
+
+
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Priority, PriorityAdmin)
+
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Priority(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    deadline_date = models.DateField(null=True, blank=True,
+                                    db_index=True)
+    deadline_time = models.TimeField(null=True, blank=True)
+    content = models.CharField(max_length=7000)
+    status = models.BooleanField(default=False)
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE,
+                                 default=None)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE,
+                                default=None)
+    class Meta:
+        verbose_name = 'Задачу'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
+
+
+
