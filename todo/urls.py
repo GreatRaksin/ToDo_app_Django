f8197d0a33af38e3dc02876e812e418c50533b3b
greatraksin <greatraksin@icloud.com>
2023-03-18 15:59:02 +0300

    Adding todo_lists views

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,7 @@ from . import views  # из той же папки импортируем фай
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lists/', views.todo_lists, name='lists'),  # полный список всех туду-листов пользователя
+    path('new_list/', views.create_list, name='new_list'),  # страница создания новго списка дел
+    path('list/<str:title>', views.show_list, name='current_list'),  # страница с задачами из списка дел
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import TasksList, Task
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='/login')
+def todo_lists(request):
+    current_user = request.user  # фиксируем пользователя, который залогинился на сайте
+    todo_list = TasksList.objects.filter(user=current_user)
+    return render(request, 'todo_lists.html', {'lists': todo_list})
+
+
+def create_list(request):
+    pass
+
+
+def show_list(request, title):
+    task_list = get_object_or_404(TasksList, title=title)
+    todos = Task.objects.filter(todo_list=task_list)
+    return render(request, 'list_detail.html', {'todos': todos})
