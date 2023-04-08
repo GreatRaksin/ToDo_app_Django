da0176863cce23f70c17ff4c06567b84ee19c6ae
greatraksin <greatraksin@icloud.com>
2023-04-08 15:57:59 +0300

    Adding emal send

--- ToDo_app_Django/settings.py
+++ ToDo_app_Django/settings.py
@@ -159,3 +159,9 @@ TINYMCE_DEFAULT_CONFIG = {
     "language": "ru",  # To force a specific language instead of the Django current language.
 }
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from .models import Contact
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -9,5 +10,10 @@ def contact(request):
     if form.is_valid():
         msg = form.save(commit=False)
         msg.save()
+        send_mail(
+            msg.subject, msg.message,
+            msg.email, ['legorobotsbel@gmail.com'],
+            fail_silently=False,
+        )
         return redirect('contact')
     return render(request, 'form.html', {'title': 'Contact us', 'form': form})
