7109d07ba3a63e17dadb4c1e3ccc17a2fb05854d
greatraksin <greatraksin@icloud.com>
2023-04-15 16:05:32 +0300

    Rearranging admin panel form fields

--- ToDo_app_Django/settings.py
+++ ToDo_app_Django/settings.py
@@ -161,8 +161,8 @@ TINYMCE_DEFAULT_CONFIG = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'legorobotsbel@gmail.com'
-EMAIL_HOST_PASSWORD = '1x95dqvtTe6a'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
