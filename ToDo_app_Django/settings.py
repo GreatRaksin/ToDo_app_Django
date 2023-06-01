f334ee54bd6200cd0bcbd8fe98ba4edd61e17301
greatraksin <greatraksin@icloud.com>
2023-06-01 14:18:36 +0300

    Finishing project

--- ToDo_app_Django/settings.py
+++ ToDo_app_Django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')  # ключ для сайта берется из переменных среды
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'd3m1d.pythonanywhere.com']
 
