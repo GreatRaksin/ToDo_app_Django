30661cc1a3b74acda1c51e32aac0a65958d84dc0
greatraksin <greatraksin@icloud.com>
2023-06-01 14:15:22 +0300

    Finishing project

--- ToDo_app_Django/settings.py
+++ ToDo_app_Django/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "production_static")
 
 # здесь будем хранить файлы сайта, необходимые для работы
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
